89e8e756b00fbb582ac9be4bfcd913720ac46622
Whiting <whitinghtennis@gmail.com>
2021-06-27 21:47:54 -0700

    commit tree 6cb076c5fd89ee36957bf1fadb43cdee7362427a

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = (num1*num2);
 
-// Write your code here
+let random = Math.floor(Math.random()+1);
+
+let num3 = 24;
+let num4 = 5;
+let mod = num3%num4;
+
+let max = Math.max(1, 2, 3, 20, 5);
